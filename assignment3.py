7892a657ffc02b7af83e51b69a799f408b59cf40
saurabh1202 <saurabh_nimb@yahoo.in>
2018-11-24 15:48:44 -0500

    read images with cv2 but not numpy yet

--- assignment3.py
+++ assignment3.py
@@ -1,8 +1,31 @@
 ###############################################
   #Assignment-3
-  #zero Shot Super Resolution Using CNN
+  #Zero Shot Super Resolution Using CNN
   #Authored By - Saurabh Nimbalkar & Anish Kale		
 ###############################################
+import numpy
+import cv2
+import glob
+
+############## READING IMAGE ###############
+
+def read_image_files():
+	images = glob.glob("*.jpg")
+	return images
+
+
+def display_images(images):
+	for image in images:
+		img = cv2.imread(image)
+		print(img,"-----------")
+	
+images = read_image_files()
+
+
+#print (images)
+
+display_images(images)
+
 
 
 
