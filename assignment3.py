c921364cdafc1afe0f05252937a7cee4010c5f52
saurabh1202 <saurabh_nimb@yahoo.in>
2018-11-25 06:00:48 -0500

    Done till padding with zero

--- assignment3.py
+++ assignment3.py
@@ -3,7 +3,8 @@
   #Zero Shot Super Resolution Using CNN
   #Authored By - Saurabh Nimbalkar & Anish Kale		
 ###############################################
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 import glob
 
@@ -14,17 +15,42 @@ def read_image_files():
 	return images
 
 
-def display_images(images):
-	for image in images:
-		img = cv2.imread(image)
-		print(img,"-----------")
+def image_to_numpy(images):
+	img = cv2.imread(image)
+	im1 = np.array(img)
+		
+	"""B = im1.copy()
+	G = im1.copy()
+	R = im1.copy()
+	B[:,:,1] = 0
+	B[:,:,2] = 0
+	G[:,:,0] = 0
+	G[:,:,2] = 0
+	R[:,:,0] = 0
+	R[:,:,1] = 0
+	print (B.shape)
+	exit()
 	
+	print(B,"\n----------")
+	print(G,"\n----------")
+	print(R,"\n----------")
+	print("\n***************\n")
+	#return B,G,R
+	"""
+	return im1
+def padwithzero(vector, pad_width, iaxis, kwargs):
+	vector[:pad_width[0]] = 0
+	vector[-pad_width[1]:] = 0
+	return vector
+	
+		
 images = read_image_files()
 
-
-#print (images)
-
-display_images(images)
+for image in images:
+	im1 = image_to_numpy(image)
+	tmp_mainImg = np.pad(im1, 1, padwithzero)
+	print(tmp_mainImg.shape)
+	exit()
 
 
 
