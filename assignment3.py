15421c9a0688d138eacfbeb7415c19fbeaa6c7e8
saurabh1202 <saurabh_nimb@yahoo.in>
2018-11-25 19:50:00 -0500

    downsampled the image by 7 steps

--- assignment3.py
+++ assignment3.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import glob
+import scipy as sc
 
 ############## READING IMAGE ###############
 
@@ -42,14 +43,28 @@ def padwithzero(vector, pad_width, iaxis, kwargs):
 	vector[:pad_width[0]] = 0
 	vector[-pad_width[1]:] = 0
 	return vector
+
+def downsample_using_bicubic_intpln(img):
+	down_sampled_list = []
+	down_sampled_image = img
+	for i in range(1,8):
+		down_sampled_image = cv2.resize(down_sampled_image, dsize=(down_sampled_image.shape[1]-1, down_sampled_image.shape[0]-1), interpolation=cv2.INTER_CUBIC)
+		down_sampled_list.append(down_sampled_image)
 	
-		
+	return down_sampled_list
+	
 images = read_image_files()
 
 for image in images:
-	im1 = image_to_numpy(image)
-	tmp_mainImg = np.pad(im1, 1, padwithzero)
-	print(tmp_mainImg.shape)
+	img = image_to_numpy(image)
+	#img1 = np.pad(img, 1, padwithzero)
+	#print(img.shape)
+	down_sampled_list = downsample_using_bicubic_intpln(img)
+	#cv2.imshow("original image",img)
+	#cv2.waitKey(0)
+	#cv2.imshow("downsampled by 7 steps",down_sampled_list[-1])
+	#cv2.waitKey(0)
+	print(down_sampled_list[-1].shape)
 	exit()
 
 
